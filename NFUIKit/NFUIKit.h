cfa8882ce356d15fa7d9f41eb3d7c2a5d16ae37a
Nikita Filonov <iamnikitafilonov@gmail.com>
2021-09-19 18:31:17 +0500

    initial

---
+++ NFUIKit/NFUIKit.h
@@ -0,0 +1,18 @@
+//
+//  NFUIKit.h
+//  NFUIKit
+//
+//  Created by Никита Филонов on 19.09.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NFUIKit.
+FOUNDATION_EXPORT double NFUIKitVersionNumber;
+
+//! Project version string for NFUIKit.
+FOUNDATION_EXPORT const unsigned char NFUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NFUIKit/PublicHeader.h>
+
+
